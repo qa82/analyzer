781761fb1d67c6685454f27a885f8d8cbc00a839
Pascal Giessler <pmgiessler@googlemail.com>
2014-12-09 14:10:11 +0100

    Renamed classes for the JAX-RS parser implementation and modified the regular expressions for a better recognition. Furthermore I added a new test case for classes with an underscore in its name.

--- server/src/main/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JaxRS_Parser.java
+++ server/src/main/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JaxRS_Parser.java
@@ -1,41 +1,165 @@
 package org.qa82.analyzer.core.providers.java.parser.jaxrs;
 
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
-import org.qa82.analyzer.core.providers.java.parser.Parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * This interface represents the method definitions, which should be implemented in any JAX RS parser
+ * This class is responsible for parsing JAX RS services made with the Jersey framework in version 2.
  * Created by Pascal Giessler on 04.12.14.
  */
-public interface JaxRS_Parser extends Parser {
+public class JaxRs_Parser implements JaxRs_Compatibility {
+
+    private ArrayList<HTTPMethods> supportedMethods;
+    private String resourceName;
+    private String className;
+    private Logger logger = Logger.getLogger(JaxRs_Parser.class.getName());
+
+    @Override
+    public String getSupportedJaxRSVersion() {
+        return "2.0";
+    }
+
+    @Override
+    public String getSupportedReferenceImplementation() {
+        return "Jersey";
+    }
+
+    @Override
+    public String getResourceName() {
+        return this.resourceName;
+    }
+
+    @Override
+    public ArrayList<HTTPMethods> getSupportedMethods() {
+        return this.supportedMethods;
+    }
+
+    @Override
+    public String getClassName() {
+        return this.className;
+    }
 
     /**
-     * This method returns supported version number of the JAX RS reference implementation
-     * @return supported version number
+     * This method is responsible for starting the parser
+     *
+     * @param file The File that should be parsed
      */
-    public String getSupportedJaxRSVersion();
+    public void parseFile(File file) {
+        this.iterateOverAllLinesOfFile(file);
+    }
 
     /**
-     * This method returns the name of the supported JAX RS reference implemenation.
-     * @return name of the JAX RS reference implementation
+     * This is the default constructor of the Jersey JAX RS Parser, which is responsible for initialization of important instance variables
      */
-    public String getSupportedReferenceImplementation();
+    public JaxRs_Parser() {
+        this.supportedMethods = new ArrayList<>();
+    }
 
     /**
-     * This method return the name of the parsed java class
-     * @return name of the parsed java class
+     * This methods iterates over all lines of the given file object
+     *
+     * @param file the files, which shall be analyzed
      */
-    public String getClassName();
+    private void iterateOverAllLinesOfFile(File file) {
+        try {
+            for (String line : Files.readAllLines(file.toPath())) {
+                this.checkForResourceClass(line);
+                if (this.isThisAResourceClass()) {
+                    this.extractClassName(line);
+                    this.checkForHTTPMethods(line);
+                }
+            }
+        } catch (IOException e) {
+            logger.debug("Error while iterating over the lines of " + file.toPath());
+        } catch (NullPointerException e) {
+            logger.error("You have to provide a file object");
+        }
+    }
 
     /**
-     * This method returns the name of the resource
-     * @return the name of the resource
+     * Extract the classname of the parsed class
+     *
+     * @param line The line, which should be checked for a class name
      */
-    public String getResourceName();
+    private void extractClassName(String line) {
+        Pattern classNamePattern = Pattern.compile("class ([a-zA-Z0-9_-]*)");
+        Matcher matcherForClassNamePattern = classNamePattern.matcher(line);
+        if (matcherForClassNamePattern.find()) {
+            this.setClassName(matcherForClassNamePattern.group(1));
+        }
+    }
 
     /**
-     * This method returns the supported methods of the current ressource
-     * @return the supported HTTP methods
+     * This method checks, if the given line contains a resource definition
+     *
+     * @param line The line, which should be checked
      */
-    public ArrayList<HTTPMethods> getSupportedMethods();
+    void checkForResourceClass(String line) {
+        Pattern pathAnnotationPattern = Pattern.compile("@Path\\(\"([a-zA-Z0-9_-]*)\"\\)");
+        Matcher matcherForPathAnnotationPattern = pathAnnotationPattern.matcher(line);
+        if (matcherForPathAnnotationPattern.find()) {
+            this.setResourceName(matcherForPathAnnotationPattern.group(1));
+        }
+    }
+
+    /**
+     * This method checks, if the given line contains a definition for HTTP methods
+     *
+     * @param line The line, which should be checked
+     */
+    void checkForHTTPMethods(String line) {
+        if (this.matchRegularExpression("@GET", line)) {
+            this.getSupportedMethods().add(HTTPMethods.GET);
+        }
+        if (this.matchRegularExpression("@POST", line)) {
+            this.getSupportedMethods().add(HTTPMethods.POST);
+        }
+        if (this.matchRegularExpression("@PUT", line)) {
+            this.getSupportedMethods().add(HTTPMethods.PUT);
+        }
+        if (this.matchRegularExpression("@DELETE", line)) {
+            this.getSupportedMethods().add(HTTPMethods.DELETE);
+        }
+    }
+
+    /**
+     * This method is a generic for matching single regular expressions of given value
+     *
+     * @param regularExpression the pattern, which is written in a regular expression
+     * @param value             the string, which should be checked
+     * @return True, if the pattern was matched in the given value
+     */
+    private boolean matchRegularExpression(String regularExpression, String value) {
+        Pattern pattern = Pattern.compile(regularExpression);
+        Matcher matcher = pattern.matcher(value);
+        return matcher.find();
+    }
+
+    /**
+     * This methods examines, if the current file is a resource class or not
+     *
+     * @return True, if the current file is a resource class
+     */
+    boolean isThisAResourceClass() {
+        return this.getResourceName() != null;
+    }
+
+    private void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    private void setClassName(String className) {
+        this.className = className;
+    }
+
+/*    public static void main (String[] args) {
+        Jersey_JaxRS_Parser parser = new Jersey_JaxRS_Parser();
+        parser.parseFile(new File("/Users/pascalgiessler/Developer/KIT/analyzer/server/src/test/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JerseyParserTestFile.java"));
+    }*/
 }
--- server/src/test/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JaxRs_ParserTestFile.java
+++ server/src/test/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JaxRs_ParserTestFile.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.MediaType;
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
-public class JerseyParserTestFile {
+public class JaxRs_ParserTestFile {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
