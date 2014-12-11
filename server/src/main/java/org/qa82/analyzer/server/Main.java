587388b8cfe8ff4bec10d9b72dcf06c6286a52a7
Pascal Giessler <pmgiessler@googlemail.com>
2014-12-11 10:21:02 +0100

    Implemented ResponseContainerFilter for Jersey to allow http requests from another source.

--- server/src/main/java/org/qa82/analyzer/server/Main.java
+++ server/src/main/java/org/qa82/analyzer/server/Main.java
@@ -34,6 +34,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers in qa82 package
         final ResourceConfig rc = new ResourceConfig().packages("org.qa82.analyzer.server");
+        rc.register(org.qa82.analyzer.server.ResponseContainerCorsFilter.class);
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
---
+++ server/src/main/java/org/qa82/analyzer/server/ResponseContainerCorsFilter.java
@@ -0,0 +1,20 @@
+package org.qa82.analyzer.server;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+/**
+ * Created by Pascal Giessler on 11.12.2014.
+ */
+public class ResponseContainerCorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type, Accept");
+    }
+}
