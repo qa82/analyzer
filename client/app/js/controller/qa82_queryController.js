aa63354477493154b9c491e2f4d13f5651b2ed3d
Pascal Giessler <pmgiessler@googlemail.com>
2014-12-11 16:57:01 +0100

    Added new controller to index.html and updated the current query controller to work with new result controller

--- client/app/js/controller/qa82_queryController.js
+++ client/app/js/controller/qa82_queryController.js
@@ -1,14 +1,27 @@
 //var qa82analyzer = angular.module('qa82analyzer', ['http']);
 
-qa82analyzer.controller('QA82_QueryController', function ($scope, httpService) {
-    $scope.qualityAnalysisList = [
-        {'name' : 'analysis1 name'},
-        {'name' : 'analysis2 name'},
-    ];
-    $scope.resultAvailable = false;
-    $scope.selectedQualityAnalysis = $scope.qualityAnalysisList[0];
+qa82analyzer.controller('QA82_QueryController', function ($rootScope, $scope, httpService) {
+    $scope.qualityAnalysisList = [];
+    $scope.selectedQualityAnalysis = undefined;
     $scope.analyze = function() {
-        console.log("Sorry not yet implemented")
+        httpService.httpPost("analyzer-test/information",$scope.selectedQualityAnalysis, function(data) {
+            $rootScope.$broadcast('resultAvailableEvent', data);
+        }, function() {
+            console.log("Error");
+        });
     };
+
+    $scope.getProvidedInformationNeed = function() {
+        httpService.httpGet("analyzer-test/informationneeds/provided", function(data) {
+            $scope.qualityAnalysisList = data;
+            console.log($scope.qualityAnalysisList);
+        }, function() {
+            console.log("Error");
+        });
+    }
+
+    $scope.init = function() {
+        $scope.getProvidedInformationNeed();
+    }
 });
 
