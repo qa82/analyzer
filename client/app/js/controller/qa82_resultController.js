8b5dfb42ad8c474f3d1cdc936b6e458509789544
Pascal Giessler <pmgiessler@googlemail.com>
2014-12-13 00:05:06 +0100

    Added function to the result controller, which will create a table structure after getting the result of an analysis

--- client/app/js/controller/qa82_resultController.js
+++ client/app/js/controller/qa82_resultController.js
@@ -4,9 +4,19 @@
 
 qa82analyzer.controller('QA82_ResultController', function ($scope) {
     $scope.resultAvailable = false;
+    $scope.columns = [];
+    $scope.rows = [];
 
     $scope.$on('resultAvailableEvent', function(event, args) {
         $scope.resultAvailable = true;
-        console.log(args);
+        if (args.information.length > 0) {
+            $scope.columns.push(args.information[0].name);
+            for (index in args.information) {
+                $scope.rows.push(args.information[index]);
+            }
+            ;
+        }
+        console.log($scope.columns);
+        console.log($scope.rows);
     });
 });
