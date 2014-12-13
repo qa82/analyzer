51754124f70a5959ad90ae844db30cf75cc45e99
Pascal Giessler <pmgiessler@googlemail.com>
2014-12-13 11:59:40 +0100

    Modified the resource name of jax-rs test file 2, so that it is different to test file 1

--- server/src/test/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JaxRsParserTestFile2.java
+++ server/src/test/java/org/qa82/analyzer/core/providers/java/parser/jaxrs/JaxRsParserTestFile2.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.MediaType;
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("myresource2")
 public class JaxRsParserTestFile2 {
 
     /**
