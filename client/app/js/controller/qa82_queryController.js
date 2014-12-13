376394c75bfd47db4305a77771d156857a30826d
Pascal Giessler <pmgiessler@googlemail.com>
2014-12-13 11:58:48 +0100

    Updated query and result controller to show the analysis results in a table

--- client/app/js/controller/qa82_queryController.js
+++ client/app/js/controller/qa82_queryController.js
@@ -1,10 +1,16 @@
 //var qa82analyzer = angular.module('qa82analyzer', ['http']);
 
 qa82analyzer.controller('QA82_QueryController', function ($rootScope, $scope, httpService) {
+
+    var BASE_URI = "analyzer-test/";
+    var INFORMATION_RESOURCE_URI = BASE_URI + "information/";
+    var INFORMATIONNEED_PROVIDED_RESOURCE_URI = BASE_URI + "informationneeds/provided/";
+
+
     $scope.qualityAnalysisList = [];
     $scope.selectedQualityAnalysis = undefined;
     $scope.analyze = function() {
-        httpService.httpPost("analyzer-test/information",$scope.selectedQualityAnalysis, function(data) {
+        httpService.httpPost(INFORMATION_RESOURCE_URI, $scope.selectedQualityAnalysis, function (data) {
             $rootScope.$broadcast('resultAvailableEvent', data);
         }, function() {
             console.log("Error");
@@ -12,7 +18,7 @@ qa82analyzer.controller('QA82_QueryController', function ($rootScope, $scope, ht
     };
 
     $scope.getProvidedInformationNeed = function() {
-        httpService.httpGet("analyzer-test/informationneeds/provided", function(data) {
+        httpService.httpGet(INFORMATIONNEED_PROVIDED_RESOURCE_URI, function (data) {
             $scope.qualityAnalysisList = data;
             console.log($scope.qualityAnalysisList);
         }, function() {
--- client/app/js/controller/qa82_resultController.js
+++ client/app/js/controller/qa82_resultController.js
@@ -3,20 +3,28 @@
  */
 
 qa82analyzer.controller('QA82_ResultController', function ($scope) {
+
+    var information = {};
+
     $scope.resultAvailable = false;
     $scope.columns = [];
     $scope.rows = [];
 
+
     $scope.$on('resultAvailableEvent', function(event, args) {
         $scope.resultAvailable = true;
+
+        if ($scope.rows.length == 0) {
+            for (index in args.information) {
+                $scope.rows[index] = [];
+            }
+        }
+
         if (args.information.length > 0) {
             $scope.columns.push(args.information[0].name);
             for (index in args.information) {
-                $scope.rows.push(args.information[index]);
+                $scope.rows[index].push(args.information[index]);
             }
-            ;
         }
-        console.log($scope.columns);
-        console.log($scope.rows);
     });
 });
